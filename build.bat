24181b1e74782c6fef3541367f78fdcb1a13b562
cinethezs <cinethezs@proton.me>
2026-04-15 13:36:55 +0200

    Update build scripts for local development

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 set JAVA_HOME=C:\Program Files\Android\Android Studio\jbr
-cd /d %USERPROFILE%\git\simpledit
-call %USERPROFILE%\git\simpledit\gradlew.bat assembleDebug
+cd /d "%~dp0"
+call gradlew.bat assembleRelease
