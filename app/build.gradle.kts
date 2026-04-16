b293a50535fca22b4ffb66d031ff61064382f9c6
cinethezs <cinethezs@proton.me>
2026-04-16 10:56:12 +0200

    Bump version to 2.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.photonlab"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "2.0.2"
+        versionCode = 9
+        versionName = "2.0.3"
     }
 
     buildTypes {
